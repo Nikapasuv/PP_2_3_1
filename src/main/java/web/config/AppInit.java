30a2d571a22ac127f8a6f273d0fea75c4fdc150b
Nikapasuv <k.kuchmistov@yandex.ru>
2024-05-02 20:00:31 +0300

    second commit

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,32 +1,3 @@
-//package web.config;
-//
-//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-//
-//public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
-//
-//    // Метод, указывающий на класс конфигурации
-//    @Override
-//    protected Class<?>[] getRootConfigClasses() {
-//        return null;
-//    }
-//
-//
-//    // Добавление конфигурации, в которой инициализируем ViewResolver, для корректного отображения jsp.
-//    @Override
-//    protected Class<?>[] getServletConfigClasses() {
-//        return new Class<?>[]{
-//                WebConfig.class
-//        };
-//    }
-//
-//
-//    /* Данный метод указывает url, на котором будет базироваться приложение */
-//    @Override
-//    protected String[] getServletMappings() {
-//        return new String[]{"/"};
-//    }
-//
-//}
 package web.config;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
@@ -63,7 +34,7 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     public void onStartup(ServletContext servletContext) throws ServletException {
         super.onStartup(servletContext);
 
-        // исправляем проблемы с кириллицей
+
         FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
         encodingFilter.setInitParameter("encoding", "UTF-8");
         encodingFilter.setInitParameter("forceEncoding", "true");
